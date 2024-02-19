bbd09f19868b76899eadf2aaea4bdcc05b5725f7
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-19 10:22:14 +0300

    configured database connection

---
+++ chatBot/src/test/java/com/bootcamp/application/chatBot/ChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.application.chatBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
